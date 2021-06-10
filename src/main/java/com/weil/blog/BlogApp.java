15a92b9a33d2313df8c02ce29969d010d8ee3bd9
wangchao <wangchao@spinfosec.com>
2021-06-10 14:38:59 +0800

    初始提交

---
+++ src/main/java/com/weil/blog/BlogApp.java
@@ -0,0 +1,18 @@
+package com.weil.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName BlogApp
+ * @Author weil
+ * @Description //启动类
+ * @Date 2021/6/10 13:53
+ * @Version 1.0.0
+ **/
+@SpringBootApplication
+public class BlogApp {
+    public static void main(String[] args) {
+        SpringApplication.run(BlogApp.class, args);
+    }
+}
